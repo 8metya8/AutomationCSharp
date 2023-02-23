565c999eaed948a2d465e838cd41d721dd9d30ea
Andrei Metelski <8metya8@gmail.com>
2023-02-23 15:10:49 +0300

    Added Task 2 - Ranges

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Task1();
+            Task2();
         }
 
         public static void Task1()
@@ -37,16 +38,13 @@
                 case "+":                    
                     Console.WriteLine($"{operand1} + {operand2} = {operand1 + operand2}");
                     break;
-
                 case "-":
                     Console.WriteLine($"{operand1} - {operand2} = {operand1 - operand2}");
                     break;
-
                 case "*":
                     Console.WriteLine($"{operand1} * {operand2} = {Math.Round(operand1 * operand2, 6)}");
                     Console.WriteLine("*The result has been round to 6 digits after zero.");
                     break;
-
                 case "/":
                     if (operand2 != 0)
                     {
@@ -56,14 +54,53 @@
                     else
                     {
                         Console.WriteLine("Division by zero!");
-                    }
-                    
+                    }                    
                     break;
-
                 default: 
                     Console.WriteLine("You selected undefined operation.");
                     break;
             }
+        }
+
+        public static void Task2()
+        {
+            // Напишите программу определения, попадает ли указанное пользователем число от 0 до 100 в числовой промежуток[0 - 14][15 - 35][36 - 49][50 - 100].
+            // Если да, то укажите, в какой именно промежуток.
+            // Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков, то выводится соответствующее сообщение.
+
+            Console.WriteLine("----- Task 2 \"Ranges\" -----\n");
+            Console.Write("Enter a value from 0 to 100:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if ((number >= 0) & (number <= 100))
+                {
+                if ((number >= 0) & (number <= 14))
+                {
+                    Console.Write("The value is in range [0 - 14].");
+                }
+                else if (number <= 35)
+                {
+                    Console.Write("The value is in range [15 - 35].");
+                }
+                else if (number <= 49)
+                {
+                    Console.Write("The value is in range [36 - 49].");
+                }
+                else if (number <= 100)
+                {
+                    Console.Write("The value is in range [50 - 100].");
+                }
+                else
+                {
+                    Console.Write("The value is in no one of range.");
+                }
+            }
+            else
+            {
+                Console.Write("The value is not in range [0 - 100].");
+            }
+                                                           
         }
 
     }
