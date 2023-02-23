2f55f84e5d81f5a3fe5a4758210ed5d6a858c441
Andrei Metelski <8metya8@gmail.com>
2023-02-23 15:40:01 +0300

    Add Task 3 - Dictionary

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -6,6 +6,7 @@
         {
             Task1();
             Task2();
+            Task3();
         }
 
         public static void Task1()
@@ -101,6 +102,59 @@
                 Console.Write("The value is not in range [0 - 100].");
             }
                                                            
+        }
+
+        public static void Task3()
+        {
+            // Напишите программу русско - английский переводчик.
+            // Программа знает 10 слов о погоде.
+            // Требуется, чтобы пользователь вводил слово на русском языке, а программа давала ему перевод этого слова на английском языке.
+            // Если пользователь ввел слово, для которого отсутствует перевод, то следует вывести сообщение, что такого слова нет.
+            
+            //Set encoding for russian symbols
+            Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+
+            Console.WriteLine("----- Task 3 \"Dictionary\" -----\n");
+            Console.Write("Enter russion word (Topic is the weather.): ");
+            string word = Console.ReadLine().ToLower();
+
+            switch(word)
+            {
+                case "дождь":
+                    Console.WriteLine("\nRUS Дождь - ENG Rain");
+                    break;
+                case "снег":
+                    Console.WriteLine("\nRUS Снег - ENG Snow");
+                    break;
+                case "солнце":
+                    Console.WriteLine("\nRUS Солнце - ENG Sun");
+                    break;
+                case "туман":
+                    Console.WriteLine("\nRUS Туман - ENG Fog");
+                    break;
+                case "облако":
+                    Console.WriteLine("\nRUS Облако - ENG Cloud");
+                    break;
+                case "ветер":
+                    Console.WriteLine("\nRUS Ветер - ENG Wind");
+                    break;
+                case "небо":
+                    Console.WriteLine("\nRUS Небо - ENG Sky");
+                    break;
+                case "холодно":
+                    Console.WriteLine("\nRUS Холодно - ENG Cold");
+                    break;
+                case "жарко":
+                    Console.WriteLine("\nRUS Жарко - ENG Hot");
+                    break;
+                case "тепло":
+                    Console.WriteLine("\nRUS Тепло - ENG Warm");
+                    break;
+                default:
+                    Console.WriteLine($"\nDictionary doesn't contain word '{word}'.");
+                    break;
+            }
         }
 
     }
