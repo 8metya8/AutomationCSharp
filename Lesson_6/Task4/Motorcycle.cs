b3e3d197c2c0bd9c80e6a7bcce1b2a63813a344d
Andrei Metelski <8metya8@gmail.com>
2023-03-13 18:03:17 +0300

    Lesson 6-7 Added Task 4

---
+++ Lesson_6/Task4/Motorcycle.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson_6
+{
+    internal class Motorcycle : Auto
+    {
+        public bool HasSidecar { get; set; }
+        public Motorcycle(string brand, string number, int speed, double loadCapacity, bool hasSidecar)
+        {
+            Brand = brand;
+            Number = number;
+            Speed = speed;
+            LoadCapacity = loadCapacity;
+            HasSidecar = hasSidecar;
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"{this.GetType().Name} \nBrand - {Brand} \nNumber - {Number} \nSpeed - {Speed} km/h \nHas sidecar - {HasSidecar} \nLoadCapacity - {GetLoadCapacity()} ton(s).");
+        }
+
+        public override double GetLoadCapacity()
+        {
+            return HasSidecar ? LoadCapacity : 0;
+        }
+    }
+}
