c9f7d51ee47665656c1e04c7364ed16e0e670ab1
Andrei Metelski <8metya8@gmail.com>
2023-03-20 21:42:11 +0300

    Added HW Lesson 9 - Task 0

---
+++ Lesson_9/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HW Task 0
+            Task0.Task_ArrayException();
+            Task0.Task_Dictionary();
+
+            //HW Task 1
+        }
+    }
+}
