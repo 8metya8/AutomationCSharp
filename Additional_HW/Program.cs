3afd2913c000189a436c2bfe854e6e7bf002c829
Andrei Metelski <8metya8@gmail.com>
2023-03-28 23:48:08 +0300

    Added Task 2

--- Additional_HW/Program.cs
+++ Additional_HW/Program.cs
@@ -3,9 +3,14 @@
     internal class Program
     {
         static void Main(string[] args)
-        {           
+        {
+            Console.WriteLine("Task 1 - Second max elemnt of array.");
             Task1.MaxAndSecondMax(new int[] { 2, 1, 5, 3, 4, 5});
             Task1.MaxAndSecondMax(new int[] { 8, 1, 15, 10, 7, 5, 11, 13});
+
+            Console.WriteLine("Task 2 - Order array consisting of 0's and 1's.");
+            Task2.OrderArray(new int[] { 0, 1, 0, 1 });
+            Task2.OrderArray(new int[] { 1, 1, 0, 1, 1, 1, 0 });
         }
     }
 }
---
+++ Additional_HW/Task2.cs
@@ -0,0 +1,46 @@
+﻿namespace Additional_HW
+{
+    /// <summary>
+    /// Order array consisting of 0's and 1's, so that: 
+    /// - 1's are on the right 
+    /// - 0's are on the left 
+    ///
+    /// Examples: 
+    /// Before [0, 1, 0 ,1] 
+    /// After [0, 0, 1, 1] 
+    /// Before [1, 1, 0, 1, 1, 1, 0] 
+    /// After [0, 0, 1, 1, 1, 1, 1]
+    /// </summary>
+    internal class Task2
+    {
+        /// <summary>
+        /// Show new ordered array: 1's are on the right and 0's are on the left 
+        /// </summary>
+        /// <param name="intArray"></param>
+        public static void OrderArray(int[] intArray)
+        {
+            //Get count of zero in array and print old array
+            Console.Write("\nOld array - ");
+            int countOfZero = 0;
+            foreach(int i in intArray)
+            {
+                if(i == 0)
+                {
+                    countOfZero++;
+                }
+
+                Console.Write(i + " ");
+            }
+
+            //Add zeros in start of new array and print it
+            Console.Write("\nNew array - ");
+            int[] orderedArray = new int[intArray.Length];
+            for(int i = 0; i < orderedArray.Length; i++)
+            {
+                orderedArray[i] = countOfZero > 0 ? 0 : 1;
+                countOfZero--;
+                Console.Write(orderedArray[i] + " ");
+            }
+        }
+    }
+}
